b2587bb0ca7c7a1a48ffdc94e69a26d8e81a16ff
zachabrahams11@gmail.com <zatguy79>
2026-09-23 14:24:21 -0500

    task 1: update device

--- app.py
+++ app.py
@@ -38,3 +38,11 @@ def create_device(device: Device):
     new_device = device.model_dump()
     readings.append(new_device)
     return new_device
+
+@app.put("/devices/{name}")
+def put_device(name: str, updated_device: Device):
+    for index, device in enumerate(readings):   #enumerate means u get the index of readings along with the device
+        if device["name"] == name:                 #device["name"] doesn't work as it isn't a dictionary
+            readings[index] = updated_device.model_dump()   #.model_dump() changes it to a dictionary
+            return readings[index]
+    raise HTTPException(status_code=404, detail="No device called " + name)
